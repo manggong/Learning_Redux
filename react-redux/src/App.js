c89b18f9aa5c94806d97a3eb6570a16ccdc40d2e
manggong <jihwank1995@gmail.com>
2020-08-02 22:48:39 +0900

    Add statement components

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class AddNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input type="button" value="+"></input>
+        <input type="text" value="0"></input>
+      </div>
+    );
+  }
+}
+
 class AddNumberRoot extends Component {
   render() {
     return (
       <div>
         <h1>Add Numnber Root</h1>
+        <AddNumber />
       </div>
     );
   }
@@ -16,8 +29,31 @@ function App() {
     <div className="App">
       <h1>Root</h1>
       <AddNumberRoot />
+      <DisplayNumberRoot />
     </div>
   );
+}
+
+class DisplayNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Display Number</h1>
+        <input type="text" value="0" readOnly></input>
+      </div>
+    );
+  }
+}
+
+class DisplayNumberRoot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Display Number Root</h1>
+        <DisplayNumber />
+      </div>
+    );
+  }
 }
 
 export default App;
