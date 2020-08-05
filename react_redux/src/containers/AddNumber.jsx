a81fc82937120753e9b4c728b464d6d355e1d2b8
manggong <jihwank1995@gmail.com>
2020-08-05 20:12:57 +0900

    Modify dir name

---
+++ react_redux/src/containers/AddNumber.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import store from '../store';
+import AddNumber from '../components/AddNumber';
+
+export default class extends Component {
+  render() {
+    return (
+      <AddNumber
+        onClick={function(size) {
+          store.dispatch({ type: 'INCREAMENT', size });
+        }}
+      ></AddNumber>
+    );
+  }
+}
